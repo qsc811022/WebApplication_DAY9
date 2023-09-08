2a414179a1d8c5caf2491156441d04fb339f3714
劉敦桐 <zse811022@hotmail.com>
2023-09-09 07:13:06 +0800

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_DAY9.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            //ViewBag.Message = "Hello, World!";
+
+            return Content("Hello, World!");
+
+
+        }
+    }
+}
